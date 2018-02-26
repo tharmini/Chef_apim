822528c9c13c9010d7365fee35a2b6463d9debad
tharmini <tharmini7@gmail.com>
2018-02-26 13:24:21 +0530

    made some changes

--- wso2api/recipes/default.rb
+++ wso2api/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+# Cookbook:: wso2api
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'wso2api::javains'
+include_recipe 'wso2api::wso2ins'
